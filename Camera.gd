c8527d60a47c049ba5529ddcaf9ccda4118e0918
Samuel Evans-Powell <sam@livebranchstudios.com.au>
2017-08-24 15:34:19 +1000

    Custom Camera in Godot 3: Let's simplify the code!

---
+++ Camera.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+onready var screen_size = OS.get_window_size()
+onready var player = get_node("Player")
+
+func _ready():
+    update_camera()
+
+func update_camera():
+    var canvas_transform = get_viewport().get_canvas_transform()
+    canvas_transform[2] = -player.get_pos() + screen_size / 2
+    get_viewport().set_canvas_transform(canvas_transform)
